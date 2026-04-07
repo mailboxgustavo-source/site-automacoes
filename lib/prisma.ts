d25e7c6c823eda3530b7981d739e92046acf2141
Gustavo caoue <mailboxgustavo@gmail.com>
2026-04-07 17:56:49 -0300

    fix: corrigindo sintaxe do prisma e dependencias

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,13 +3,9 @@
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
+    log: ['query'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
