7f6c7a079e400068a21b81761eee94984271cb6c
Gustavo caoue <mailboxgustavo@gmail.com>
2026-04-07 22:12:24 -0300

    chore: final sets

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,17 @@
 ﻿import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof makePrisma> | undefined
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+function makePrisma() {
+  return new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL as string,
+    log: process.env.NODE_ENV === 'development' ? ['query'] : [],
+  }).$extends(withAccelerate())
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma  
+export const prisma = globalForPrisma.prisma ?? makePrisma()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
