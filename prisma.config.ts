2283c7fbd22bee4fa0e83a6317be926d3401dffc
Gustavo caoue <mailboxgustavo@gmail.com>
2026-04-07 21:44:54 -0300

    chore: atualiza config do prisma 7 e remove url do schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
   datasource: {
-    // Aqui usamos a string direta ou buscamos do process.env
-    url: "postgresql://postgres:Consta1%23%23@localhost:5432/site_automacoes",
+    url: "postgresql://neondb_owner:npg_Ygj3mrXdlz4q@ep-fancy-haze-aniuw6t4.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require",
   },
 })
